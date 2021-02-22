01c2c39359d858aa166372603d39e89110a384f7
ramesses-edu <romgrishin@gmail.com>
2021-02-22 22:15:56 +0200

    Update main.go

--- begin/main.go
+++ begin/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var ch chan int = make(chan int)
+
 func main() {
-	sw := 1
+	sw := 6
 	switch sw {
 	case 1: // Hello, NIX Education
 		task1()
@@ -25,6 +31,7 @@ func main() {
 	case 6: // work with DB
 		task6()
 	}
+	close(ch)
 }
 
 var netResource string = "https://jsonplaceholder.typicode.com/"
@@ -79,8 +86,6 @@ func task3() {
 	printScr(url)
 }
 
-var ch chan int = make(chan int)
-
 func task4() {
 	page := "posts/"
 	url := ""
@@ -115,12 +120,135 @@ func task5() {
 	//time.Sleep(time.Second * 5)
 }
 
+type workbchDB struct {
+	DBname, user, password, host string
+	link                         *sql.DB
+}
+
+func (wDB *workbchDB) connectMySQL() (*sql.DB, error) {
+	driverName := "mysql"
+	connectString := fmt.Sprintf("%s:%s@tcp(%s)/%s", wDB.user, wDB.password, wDB.host, wDB.DBname)
+	db, err := sql.Open(driverName, connectString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	wDB.link = db
+	return db, err
+}
+func (wdb *workbchDB) createTable(tname string, s interface{}) {
+	/* stmt, err := db.Prepare(createTable)
+		if err != nil {
+			panic(err)
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var createTable = `
+	CREATE TABLE IF NOT EXISTS people (
+	     user_id      INTEGER PRIMARY KEY AUTO_INCREMENT
+	    ,username     VARCHAR(32)
+	    ,phone        VARCHAR(32)
+	); */
+}
+func (wdb *workbchDB) dropTable(tname string) bool {
+	dropT := fmt.Sprintf("DROP TABLE %s;", tname)
+	stat, err := wdb.link.Prepare(dropT)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	defer stat.Close()
+	_, err = stat.Exec()
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	return true
+}
+
+type Post struct {
+	UserId int
+	Id     int
+	Title  string
+	Body   string
+}
+type Comment struct {
+	PostId int
+	Id     int
+	Name   string
+	Email  string
+	Body   string
+}
+type ElemReq interface {
+	writeDB()
+}
+
+func (p *Post) writeDB() {
+	fmt.Println(p.Id)
+}
+func (c *Comment) writeDB() {
+	fmt.Println(c.Id)
+}
+
 func task6() {
 	/*
 		   получить json posts?userId=7  обработать: получить структуру постов
-		   в горутины 1го ур выдавать по элементу(посту) структуры
+		   в горутины 1го ур(анонимные функции) выдавать по элементу(посту) структуры
 		   		в этой рутине произвести запись поста в БД, получить json comments?postId обработать: получить структуру комментов
 		   		в горутины 2го ур выдавать по элементу(комменту) структуры
 				   в этой рутине произвести запись коммента
 	*/
+	wdb := workbchDB{DBname: "edudb", user: "utest", password: "12345", host: "localhost:3306"}
+	db, _ := wdb.connectMySQL()
+	defer db.Close()
+	db
+	url := netResource + "posts?userId=7"
+	resp, _ := netRequest("get", url)
+	var posts []Post
+	err := json.Unmarshal([]byte(resp), &posts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ch1 chan int = make(chan int)
+	for _, value := range posts {
+		go procPost(&wdb, value, ch1)
+	}
+	for i := 1; i <= len(posts); {
+		i += <-ch1
+	}
+	close(ch1)
+}
+
+func write2DB(wdb *workbchDB, e ElemReq) {
+	//использовать мьютексы во время инсертов
+
+}
+
+func procPost(wdb *workbchDB, p Post, ch chan int) {
+	url := netResource + "comments?postId=" + strconv.Itoa(p.Id)
+	resp, _ := netRequest("get", url)
+	var comments []Comment
+	err := json.Unmarshal([]byte(resp), &comments)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ch2 chan int = make(chan int)
+	for _, value := range comments {
+		go procComment(wdb, value, ch2)
+	}
+	for i := 1; i <= len(comments); {
+		i += <-ch2
+	}
+	close(ch2)
+	ch <- 1
+}
+
+func procComment(wdb *workbchDB, c Comment, ch chan int) {
+	fmt.Println(c)
+	ch <- 1
 }
